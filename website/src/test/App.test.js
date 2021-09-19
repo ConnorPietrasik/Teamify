403d2e981da9ed0e4ecbcc2f66805060601fc4d0
Melanie Chan <melanie.chan@sjsu.edu>
2021-09-19 01:10:01 -0700

    started tests for login and home rendering

--- website/src/test/App.test.js
+++ website/src/test/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from '../pages/App';
+import Login from '../pages/Login';
+import Home from '../pages/Home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders login page', () => {
+  render(<Login />);
+  const linkElement = screen.getByText(/Welcome/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders home page', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
