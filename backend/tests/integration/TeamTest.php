86179250880d8050e0197849241d412d4e34f645
Connor Pietrasik <connorgpietrasik@gmail.com>
2021-10-14 21:16:39 -0700

    added GET /team/id and some tests

---
+++ backend/tests/integration/TeamTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\integration;
+
+if ( !isset( $_SESSION ) ) $_SESSION = array(  );
+
+class TeamTest extends TestCase{
+    protected $backupGlobalsBlacklist = array( '_SESSION' );
+    protected $team_id;
+
+    //Successfully registers a testuser with a password
+    public function testRegisterPass(): void {
+        $params = [
+            'username' => 'testUser',
+            'password' => 'test'
+        ];
+        $req = $this->createRequest('POST', '/register');
+        $request = $req->withParsedBody($params);
+        $response = $this->getAppInstance()->handle($request);
+
+        $result = (string) $response->getBody();
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertStringContainsString('user_id', $result);
+    }
+
+    //Successfully creates a team
+    public function testCreateTeam(): void {
+        $params = [
+            'name' => 'testTeam',
+            'description' => 'this is a test',
+            'tags' => ['test', 'testytest'],
+            'looking_for' => ['testing', 'being very good at testing']
+        ];
+        $req = $this->createRequest('POST', '/env/1/createteam');
+        $request = $req->withParsedBody($params);
+        $response = $this->getAppInstance()->handle($request);
+
+        $result = (string) $response->getBody();
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertStringContainsString('team_id', $result);
+        $this->team_id = json_decode($result)->team_id;
+    }
+
+    //Verifies that the team info is all correct
+    public function testGetTeam(): void {
+        $request = $this->createRequest('GET', '/team/'.$this->team_id);
+        $response = $this->getAppInstance()->handle($request);
+
+        $result = (string) $response->getBody();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('testTeam', $result);
+        $this->assertStringContainsString('this is a test', $result);
+        $this->assertStringContainsString('testytest', $result);
+        $this->assertStringContainsString('being very good at testing', $result);
+    }
+
+    //Successfully deletes the user
+    public function testDelete(): void {
+        $request = $this->createRequest('DELETE', '/user');
+        $response = $this->getAppInstance()->handle($request);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
