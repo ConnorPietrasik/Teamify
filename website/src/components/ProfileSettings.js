c34593d3ecc035b20ed20abd0fe0041bfd54ca70
Melanie Chan <melanie.chan@sjsu.edu>
2021-10-17 15:12:40 -0700

    added input checks in profile settings card
    reject empty inputs and invalid space character inputs for username and bio

--- website/src/components/ProfileSettings.js
+++ website/src/components/ProfileSettings.js
@@ -56,19 +56,43 @@ export default function ProfileSettings(props) {
         setEditMode(true);
   }
 
+  // when user types and input field values need to change
+  function handleInputChange(e, updateStateFunction, condition) {
+      if (condition === "no-spaces" && e.target.value.includes(" ")) { // invalid username inputs - reject space characters
+        e.target.value = e.target.value.replace(/\s/g, "");
+      }
+      else if (e.target.value.charAt(0) === " ") // invalid bio input - if first char is a space, delete it
+        e.target.value = e.target.value.substring(1);
+      else // valid inputs - update state variable
+        updateStateFunction(e.target.value);
+  }
+
   return (
     <div className="Card">
         <div>
              <button className="editBtn" onClick = {toggleEditMode}>{editMode ? "Discard Edits" : "Edit"}</button>
              <div className="nameAndPic leftAndCenter">
                <img className="profilePic" src="https://cdn-icons-png.flaticon.com/512/847/847969.png" />
-               <p>{ editMode ? <input type="text" value={username} onChange = {e => setUsername(e.target.value) }/> : username}</p>
+               <p>{ editMode ?
+                   <input type="text" value={username}
+                      onChange = {e => handleInputChange(e, setUsername, "no-spaces") }/>
+                    : username}
+               </p>
              </div>
          </div>
 
-        <div className="shiftRight">About Me: { editMode ? <input type="text" value={bio} onChange = {e => setBio(e.target.value) }/> : bio}</div>
+        <div className="shiftRight">About Me:
+                { editMode ?
+                 <input type="text" value={bio}
+                    onChange = {e => handleInputChange(e, setBio) }/>
+                 : bio}</div>
 
-     {editMode ? <button onClick = {updateUserInfo}> Update </button> : <></>}
+     {editMode ? <button onClick = {(e) => {
+         if (username !== "" && bio !== "") // only non empty inputs go through
+            updateUserInfo(e);
+         else
+             alert("Please make sure inputs are nonempty");
+     }}> Update </button> : <></>}
 
     </div>
   );
