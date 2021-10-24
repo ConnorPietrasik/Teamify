864271f4168993735f818cf0ed7ff0240a763bbe
Connor Pietrasik <connorgpietrasik@gmail.com>
2021-10-23 18:22:26 -0700

    changed get open by skill to post

--- backend/tests/integration/EnvTest.php
+++ backend/tests/integration/EnvTest.php
@@ -105,7 +105,7 @@ class EnvTest extends TestCase{
         $params = [
             'skills' =>   ['this is a skill', 'this is a second skill']
         ];
-        $req = $this->createRequest('GET', '/env/1/open/skill');
+        $req = $this->createRequest('POST', '/env/1/open/skill');
         $request = $req->withParsedBody($params);
         $response = $this->getAppInstance()->handle($request);
 
