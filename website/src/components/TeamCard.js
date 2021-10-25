960fb8621f5f11826ff20b995bb975d33565aa91
Melanie Chan <melanie.chan@sjsu.edu>
2021-10-24 22:51:36 -0700

    display list of team members on Team Card

--- website/src/components/TeamCard.js
+++ website/src/components/TeamCard.js
@@ -4,9 +4,25 @@ import '../css/Components.css';
 
 // shows info for one team
 export default function TeamCard(props) {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+      // get team members from parameter
+      setMembers(props.team.members);
+  }, [props.team]);
+
   return (
     <div className="IndividualCard">
-        <p>{props.team.name}</p>
+        <h3>{props.team.name}</h3>
+
+        { /* show list of team members */
+            members.length > 0 ?
+            <div>
+                <h4>Members:</h4>
+                {members.map((member) => <p>{member.user_id}</p>)}
+            </div>
+            : <></>
+        }
 
         { /* Request to Join button appears if team is open and user hasn't requested to join yet */
           props.status === 'open' ?
