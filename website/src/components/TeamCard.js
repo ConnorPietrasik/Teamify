e7747f586be73c4a9e67d84ebd1f3f0b53c3dd93
Melanie Chan <melanie.chan@sjsu.edu>
2021-10-24 20:36:47 -0700

    called API to send request to team

--- website/src/components/TeamCard.js
+++ website/src/components/TeamCard.js
@@ -11,6 +11,23 @@ export default function TeamCard(props) {
         { /* Request to Join button appears if team is open and user hasn't requested to join yet */
           props.status === 'open' ?
             <button className="inviteBtn colorFadeEffect" onClick = {() => {
+                // send request for currently logged in user to join this team
+                fetch(`https://api.teamify.pietrasik.top/team/${props.team.team_id}/request`, {
+                  method: 'POST',
+                  credentials: 'include',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                    message: "can i join?",
+                  })
+                }).then()
+                  .then(data => {
+                    if (data)
+                      console.log(data);
+
+                  }).catch(console.error);
+
                 // move team card from Open Teams List to Requests Sent List
                 props.updateSentRequestsList(props.team);
             }}>Request to Join</button>
