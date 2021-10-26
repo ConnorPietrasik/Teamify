8288b014118ccd0256e5e97e78a3575a3c7db098
Connor Pietrasik <connorgpietrasik@gmail.com>
2021-10-25 22:26:16 -0700

    fixed typo

--- backend/src/Controller/Team/GetRequests.php
+++ backend/src/Controller/Team/GetRequests.php
@@ -13,7 +13,7 @@ final class GetRequests extends Base {
     public function __invoke(Request $request, Response $response, array $args): Response {
 
         $requests = $this->getTeamService()->getTeamRequests((int) $args['team_id']);
-        $env_id = $this->getTeamService()->getTeamEnvironmentID((int) $args['team_id']);
+        $env_id = $this->getTeamService()->getTeamEnvID((int) $args['team_id']);
         foreach ($requests as &$req) {
             $req['user'] = $this->getEnvService()->getEnvUser($env_id, $req['user_id']);
             unset($req['user_id']);
