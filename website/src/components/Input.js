57063a968490ad826e161414583e6f69f77c1ab7
Melanie Chan <melanie.chan@sjsu.edu>
2021-10-31 19:20:13 -0700

    refactored input component
    made separate component for text inputs

---
+++ website/src/components/Input.js
@@ -0,0 +1,17 @@
+export default function LineInput(props) {
+    // when user types and input field values need to change
+    function handleInputChange(e, updateStateFunction) {
+        if (props.noSpaces && e.target.value.includes(" ")) { // invalid username inputs - reject space characters
+          e.target.value = e.target.value.replace(/\s/g, "");
+        }
+        else if (e.target.value.charAt(0) === " ") // invalid bio input - if first char is a space, delete it
+          e.target.value = e.target.value.substring(1);
+        else // valid inputs - update state variable
+          updateStateFunction(e.target.value);
+    }
+
+    return(
+        <input type="text" value={props.stateValue}
+           onChange = {e => handleInputChange(e, props.stateSetter) }/>
+    );
+}
