2e5da068588877c98f419283b6e396df7cb5406d
Connor Pietrasik <connorgpietrasik@gmail.com>
2021-11-25 01:29:05 -0800

    fixed typo

--- backend/src/Controller/Environment/GetEnvUser.php
+++ backend/src/Controller/Environment/GetEnvUser.php
@@ -9,7 +9,7 @@ use App\Helper\JsonResponse;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-final class GetOpen extends Base {
+final class GetEnvUser extends Base {
     public function __invoke(Request $request, Response $response, array $args): Response {
 
         $users = $this->getEnvService()->getEnvUser((int) $args['env_id'], (int) $args['user_id']);
